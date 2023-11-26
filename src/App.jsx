edc1b00955597c1cf38ad93101b442a88cda647e
devarthurguilherme <arthurguilherme2013@gmail.com>
2023-11-26 11:31:43 +0000

    Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 
+import Navigation from "./components/Navigation"
+
 function App() {
   return (
     <>
-      <h1 className="text-red-400">Hello World!</h1>
+      <Navigation />
       <Outlet />
       <p>Footer</p>
     </>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const Navigation = () => {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <Link to="/arthurs_library/">Home</Link>
+                <Link to="contact">Contact</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navigation
